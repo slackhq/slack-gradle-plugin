c97818f7a03f9c0b8710dc6cc0b9710b3caaf0f0
Linh Pham <lpham@slack-corp.com>
2024-03-06 13:28:07 -0800

    wip

--- skate-plugin/src/main/kotlin/com/slack/sgp/intellij/circuitgen/CreateCircuitFeatureFromTemplate.kt
+++ skate-plugin/src/main/kotlin/com/slack/sgp/intellij/circuitgen/CreateCircuitFeatureFromTemplate.kt
@@ -6,6 +6,8 @@ import com.intellij.ide.actions.CreateFileFromTemplateAction
 import com.intellij.ide.actions.CreateFileFromTemplateDialog
 import com.intellij.ide.fileTemplates.FileTemplate
 import com.intellij.ide.fileTemplates.FileTemplateManager
+import com.intellij.ide.fileTemplates.actions.AttributesDefaults
+import com.intellij.ide.fileTemplates.ui.CreateFromTemplateDialog
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiFile
@@ -25,7 +27,12 @@ class CreateCircuitFeatureFromTemplate: CreateFileFromTemplateAction("New Circui
     if (dir != null) {
       val defaultProperties = FileTemplateManager.getInstance(dir.project).defaultProperties
       val properties = Properties(defaultProperties)
-      val element = CreateFileFromTemplateDialog(/* project = */ dir.project)
+      val dialog = template?.let {
+        CreateFromTemplateDialog(dir.project, dir,
+          it,
+          AttributesDefaults(name).withFixedName(true), properties)
+      }
+      dialog?.create()?.containingFile
     }
     return super.createFileFromTemplate(name, template, dir)
   }
